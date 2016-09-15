206a06e57439998780fc475d6241d16831186fe5
Aude Lejuez <dusterherz@gmail.com>
2016-09-15 21:51:57 +0200

    Make the Cardboard for logging meat

--- Meater/app/src/main/java/com/dusterherz/meater/MainActivity.java
+++ Meater/app/src/main/java/com/dusterherz/meater/MainActivity.java
@@ -1,7 +1,10 @@
 package com.dusterherz.meater;
 
+import java.util.Calendar;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Resources res = getResources();
+        Calendar c = Calendar.getInstance();
+        int time = c.get(Calendar.HOUR_OF_DAY);
+        String currentMeal;
+        if (time < 11) {
+            currentMeal = "petit-déjeuner";
+        }
+        else if (time < 14) {
+            currentMeal = "déjeuner";
+        }
+        else if (time < 17) {
+            currentMeal = "goûter";
+        }
+        else {
+            currentMeal = "dîner";
+        }
+        String text = String.format(res.getString(R.string.log_meat), currentMeal);
+
+        TextView t = (TextView)findViewById(R.id.textview_log_meat);
+        t.setText(text);
     }
 }
